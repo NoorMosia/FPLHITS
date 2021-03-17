4d00deb6cfd4ccc182be4896db8de5840c0974ed
noor <noortshepo@gmail.com>
2021-03-17 18:37:24 +0200

    basics setup

---
+++ src/components/Transfers/Transfers.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as Styles from "./Transfers.module.css";
+
+const Transfers = () => {
+    return <div className={Styles.Transfers}>
+        Hello
+    </div>
+}
+
+export default Transfers;
